9ef135777e2dd3f13d52da8b04d33c44a3909fed
Blu3Jund <blu3jund@gmail.com>
2022-04-13 12:13:47 -0500

    project postvask automatie

---
+++ postVakAutomatie/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+
+
+    //create postvak gedeelte
+
+    //create folder based on total number of mails
+
+    //total number folder based on total number of recent hot topics
+    fn hotTopics(let hotTopic, hoeveelheid: f64){
+        let hotTopic = "test";
+        let hoeveelheid: f64 = 8.0;
+
+        return (hotTopics, hoeveelheid)
+    }
+    //versleep de mail
+
+    //
+
+}
